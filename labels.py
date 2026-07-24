3a937f0b3eb388da04558a12d005beac9cbc7859
Danixd1510 <danixaroxana@gmail.com>
2026-07-23 21:36:56 -0500

    Create labels.py

---
+++ labels.py
@@ -0,0 +1,16 @@
+ETIQUETAS = {
+    "359": "Efectivo y equivalentes", "360": "Inversiones financieras",
+    "361": "Ctas. por cobrar comerciales - terc", "362": "Ctas. por cobrar comerciales - relac",
+    "364": "Ctas. por cobrar diversas - terceros", "366": "Serv. y otros contratados por anticipado",
+    "367": "Estimación ctas. de cobranza dudosa", "368": "Mercaderías",
+    "373": "Materias aux, suministros y repuestos", "381": "Activos por derecho de uso",
+    "382": "Propiedades, planta y equipo", "383": "Depreciación acumulada",
+    "388": "Activo diferido", "401": "Sobregiros bancarios",
+    "402": "Trib. y aport. por pagar", "404": "Ctas. por pagar comerciales - terc",
+    "409": "Obligaciones financieras", "410": "Provisiones",
+    "414": "Capital", "421": "Resultados acumulados positivos",
+    "423": "Utilidad del ejercicio", "461": "Ventas netas",
+    "464": "Costo de ventas", "468": "Gastos de ventas",
+    "469": "Gastos de administración", "472": "Gastos financieros",
+    "473": "Ingresos financieros gravados", "490": "Impuesto a la renta"
+}
