8db5771abd41ed317f7deda000e3e383901dd4da
Danixd1510 <danixaroxana@gmail.com>
2026-07-23 20:17:24 -0500

    Remove clear session state button
    Remove the button to clear session state and rerun.

--- app.py
+++ app.py
@@ -49,7 +49,3 @@ if st.button("Generar Excel"):
 
         with open(nombre_final, "rb") as f:
             st.download_button("📥 Descargar Excel", f, file_name=nombre_final)
-
-if st.button("Limpiar"):
-    st.session_state.clear()
-    st.rerun()
