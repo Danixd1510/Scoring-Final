8e3fbc11e439fd32012ba10061fc79206756cd3a
Danixd1510 <danixaroxana@gmail.com>
2026-07-23 21:40:35 -0500

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,29 @@
+# Lista maestra de cuentas en el orden de tu Excel
+# Formato: (Codigo_SUNAT, Nombre_en_Excel, Seccion)
+CUENTAS = [
+    ("359", "Efectivo y equivalentes de efectivo", "Activo"),
+    ("360", "Inversiones financieras", "Activo"),
+    ("361", "Ctas. por cobrar comerciales - ter.", "Activo"),
+    ("362", "Ctas. por cobrar comerciales - relac.", "Activo"),
+    ("364", "Ctas. por cobrar diversas - terceros", "Activo"),
+    ("365", "Ctas. por cobrar diversas - relacionados", "Activo"),
+    ("366", "Serv. y otros contratados por anticipado", "Activo"),
+    ("368", "Mercaderías", "Activo"),
+    ("373", "Materias aux, suministros y repuestos", "Activo"),
+    ("381", "Activos por derecho de uso (2)", "Activo"),
+    ("382", "Propiedades, planta y equipo", "Activo"),
+    ("383", "Depreciación de 1,2 y PPE acumulados", "Activo"),
+    ("388", "Activo diferido", "Activo"),
+    
+    ("401", "Sobregiros bancarios", "Pasivo"),
+    ("402", "Trib. y aport. sist. pens. y salud por pagar", "Pasivo"),
+    ("404", "Ctas. por pagar comerciales - terceros", "Pasivo"),
+    ("405", "Ctas. por pagar comerciales - relac.", "Pasivo"),
+    ("407", "Ctas. por pagar diversas - terceros", "Pasivo"),
+    ("409", "Obligaciones financieras", "Pasivo"),
+    ("411", "Pago diferido", "Pasivo"),
+    
+    ("414", "Capital", "Patrimonio"),
+    ("421", "Resultados acumulados positivos", "Patrimonio"),
+    ("423", "Utilidad del ejercicio", "Patrimonio")
+]
