1d699c85b8e37d0ac06d503f88758768b8b3e09e
Danixd1510 <danixaroxana@gmail.com>
2026-07-23 21:48:34 -0500

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,29 +1,22 @@
-# Lista maestra de cuentas en el orden de tu Excel
-# Formato: (Codigo_SUNAT, Nombre_en_Excel, Seccion)
-CUENTAS = [
-    ("359", "Efectivo y equivalentes de efectivo", "Activo"),
-    ("360", "Inversiones financieras", "Activo"),
-    ("361", "Ctas. por cobrar comerciales - ter.", "Activo"),
-    ("362", "Ctas. por cobrar comerciales - relac.", "Activo"),
-    ("364", "Ctas. por cobrar diversas - terceros", "Activo"),
-    ("365", "Ctas. por cobrar diversas - relacionados", "Activo"),
-    ("366", "Serv. y otros contratados por anticipado", "Activo"),
-    ("368", "Mercaderías", "Activo"),
-    ("373", "Materias aux, suministros y repuestos", "Activo"),
-    ("381", "Activos por derecho de uso (2)", "Activo"),
-    ("382", "Propiedades, planta y equipo", "Activo"),
-    ("383", "Depreciación de 1,2 y PPE acumulados", "Activo"),
-    ("388", "Activo diferido", "Activo"),
-    
-    ("401", "Sobregiros bancarios", "Pasivo"),
-    ("402", "Trib. y aport. sist. pens. y salud por pagar", "Pasivo"),
-    ("404", "Ctas. por pagar comerciales - terceros", "Pasivo"),
-    ("405", "Ctas. por pagar comerciales - relac.", "Pasivo"),
-    ("407", "Ctas. por pagar diversas - terceros", "Pasivo"),
-    ("409", "Obligaciones financieras", "Pasivo"),
-    ("411", "Pago diferido", "Pasivo"),
-    
-    ("414", "Capital", "Patrimonio"),
-    ("421", "Resultados acumulados positivos", "Patrimonio"),
-    ("423", "Utilidad del ejercicio", "Patrimonio")
-]
+# structure.py
+# Define qué casillas pertenecen a cada cuadro para la vista en la App
+BALANCE_DATA = {
+    "359": "Efectivo y equivalentes", "361": "Ctas. por cobrar comerciales - terc.",
+    "364": "Ctas. por cobrar diversas - terceros", "366": "Serv. contratados por anticipado",
+    "368": "Mercaderías", "373": "Materias aux, suministros y repuestos",
+    "381": "Activos por derecho de uso (2)", "382": "Propiedades, planta y equipo",
+    "383": "Depreciación de 1,2 y PPE acumulados", "388": "Activo diferido",
+    "401": "Sobregiros bancarios", "402": "Trib. y aport. por pagar",
+    "404": "Ctas. por pagar comerciales - terceros", "409": "Obligaciones financieras",
+    "410": "Provisiones", "414": "Capital", "421": "Resultados acumulados positivos",
+    "423": "Utilidad del ejercicio"
+}
+
+RESULTADOS_DATA = {
+    "461": "Ventas netas o ing por servicios", "462": "Desc , rebajas y bonif concedidas",
+    "464": "Costo de ventas", "466": "Resultado bruto Utilidad",
+    "468": "Gastos de ventas", "469": "Gastos de administración",
+    "470": "Resultado de operación utilidad", "472": "Gastos financieros",
+    "473": "Ingresos financieros gravados", "475": "Otros ingresos gravados",
+    "490": "Impuesto a la renta", "492": "Resultado del ejercicio"
+}
